7e4e65d2e06befaa21e216f3599b2e36636a51fc
Arthur Head <arthurhead@Arthurs-MBP.lan>
2024-07-24 21:57:42 -0700

    troubleshoot users route

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -6,18 +6,28 @@ import { authenticateToken } from '../auth';
 const prisma = new PrismaClient();
 
 router.get('/:id', authenticateToken, async (req: Request, res: Response) => {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
     const user = await prisma.users.findUnique({
         where: {
             user_id: Number(id)
         }
     });
     res.json({ user, message: 'Protected route accessed!' });
+    } catch (error) {
+        res.json({ message: 'Error fetching user' });
+    }
 });
 
 router.get('/all', authenticateToken, async (req: Request, res: Response) => {
-    const users = await prisma.users.findMany();
-    res.json({ users, message: 'Protected route accessed!' });
+    try {
+        const users = await prisma.users.findMany();
+        console.log('getting all users',users);
+        res.json({ users, message: 'Protected route accessed!' });
+    } catch (error) {
+        console.log('error fetching users',error);
+        res.json({ message: 'Error fetching users' });
+    }
 });
 
 router.get('/profile/:id', authenticateToken, async (req: Request, res: Response) => {
